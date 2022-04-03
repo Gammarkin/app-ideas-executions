b6bff2b84449e02659f5bfc4670d8f007cf1ccf8
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-04-03 17:40:42 -0300

    made bin2dec

--- tier1/Bin2Dec/script.js
+++ tier1/Bin2Dec/script.js
@@ -1,15 +1,20 @@
 const floatingInput = document.getElementById("floatingInput");
 const display = document.getElementById("display");
 
+const convertBinaryToDecimal = () => {
+  return parseInt(floatingInput.value, 2);
+};
+
 const checkAndDisplay = (fun) => {
   floatValue = floatingInput.value;
   const trueOrFalse = floatValue.split("").every((e) => e === "1" || e === "0");
   if (trueOrFalse === false)
-    display.innerHTML = "error. Only 0 and 1 are accepted as input";
-  if (trueOrFalse === true) display.innerHTML = fun();
-  if (floatValue === "") display.innerHTML = 0;
+    display.innerHTML = "Error. Only 0 and 1 are accepted as input.";
+  if (trueOrFalse === true)
+    display.innerHTML = `The binary of ${floatValue} to decimal is ${fun()}!`;
+  if (floatValue === "") display.innerHTML = "";
 };
 
-floatingInput.addEventListener("keyup", (e) => {
-  checkAndDisplay();
+floatingInput.addEventListener("keyup", () => {
+  checkAndDisplay(convertBinaryToDecimal);
 });
