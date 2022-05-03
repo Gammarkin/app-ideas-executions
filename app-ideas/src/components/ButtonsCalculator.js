be65ca83073230969ee29a3e340772bd537b4976
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-05-03 18:44:11 -0300

    made calculator show numbers and erase them

--- app-ideas/src/components/ButtonsCalculator.js
+++ app-ideas/src/components/ButtonsCalculator.js
@@ -2,33 +2,58 @@ import React, {Component} from 'react';
 
 export default class ButtonsCalculator extends Component {
   render() {
+    const {display, handleErase, handleChangeDisplay} = this.props;
     return (
       <div className="calculatorButtons">
         <div className="display">
-          <span className="display-text" />
-          <button className="operatorCE">CE</button>
+          <span className="display-text">{display}</span>
+          <button onClick={handleErase} className="operatorCE">
+            CE
+          </button>
         </div>
         <div className="buttonsCalc">
-          <button className="operator">C</button>
+          <button onClick={handleErase} className="operator">
+            C
+          </button>
           <button className="operator">()</button>
           <button className="operator">%</button>
           <button className="operator">/</button>
-          <button className="number">9</button>
-          <button className="number">8</button>
-          <button className="number">7</button>
+          <button onClick={handleChangeDisplay} className="number">
+            9
+          </button>
+          <button onClick={handleChangeDisplay} className="number">
+            8
+          </button>
+          <button onClick={handleChangeDisplay} className="number">
+            7
+          </button>
           <button className="operator">*</button>
 
-          <button className="number">6</button>
-          <button className="number">5</button>
-          <button className="number">4</button>
+          <button onClick={handleChangeDisplay} className="number">
+            6
+          </button>
+          <button onClick={handleChangeDisplay} className="number">
+            5
+          </button>
+          <button onClick={handleChangeDisplay} className="number">
+            4
+          </button>
           <button className="operator">-</button>
 
-          <button className="number">3</button>
-          <button className="number">2</button>
-          <button className="number">1</button>
+          <button onClick={handleChangeDisplay} className="number">
+            3
+          </button>
+          <button onClick={handleChangeDisplay} className="number">
+            2
+          </button>
+          <button onClick={handleChangeDisplay} className="number">
+            1
+          </button>
           <button className="operator">+</button>
 
-          <button className="number">0</button>
+          <button onClick={handleChangeDisplay} className="number">
+            0
+          </button>
           <button className="operator">+/-</button>
           <button className="operator">.</button>
           <button className="operator">=</button>
--- app-ideas/src/pages/Calculator.js
+++ app-ideas/src/pages/Calculator.js
@@ -5,12 +5,60 @@ import ButtonsCalculator from '../components/ButtonsCalculator';
 import '../styles/calculator.css';
 
 export default class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: '',
+      currOperator: '',
+      prevOperator: '',
+      prevOperand: '',
+      currOperand: '',
+    };
+  }
+
+  handleChangeDisplay = ({target}) => {
+    const allNumbers = document.getElementsByClassName('number');
+    const arr = [...allNumbers].map((number) => number.innerText);
+    const clickedNumber = arr.find((number) => number === target.innerText);
+
+    this.setState((prev) => ({
+      display: prev.display + clickedNumber,
+      currOperand: clickedNumber,
+      prevOperand: prev.currOperand,
+    }));
+  };
+
+  handleErase = ({target}) => {
+    const {innerText} = target;
+    if (innerText === 'CE') {
+      this.setState({
+        display: '',
+        currOperator: '',
+        prevOperator: '',
+        prevOperand: '',
+        currOperand: '',
+      });
+    }
+    if (innerText === 'C') {
+      const {display} = this.state;
+      this.setState({
+        currOperand: '',
+        display: display.slice(0, display.length - 1),
+      });
+    }
+  };
+
   render() {
+    const {display} = this.state;
     return (
       <div>
         <Header text="Calculator" show={true} />
         <div className="divCalculator">
-          <ButtonsCalculator />
+          <ButtonsCalculator
+            handleChangeDisplay={this.handleChangeDisplay}
+            handleErase={this.handleErase}
+            display={display}
+          />
         </div>
         <Footer />
       </div>
