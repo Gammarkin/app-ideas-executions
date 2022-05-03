a50060e5fa3fb57deed783c92449fb3372b31ff7
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-05-03 17:11:47 -0300

    changed border radius visuals

--- app-ideas/src/pages/BorderRadius.js
+++ app-ideas/src/pages/BorderRadius.js
@@ -36,7 +36,8 @@ export default class BorderRadius extends Component {
     const {input1, input2, input3, input4} = this.state;
     const message = document.querySelector('.message');
 
-    message.innerText = `${input1} ${input2} ${input3} ${input4}`;
+    message.innerText = `Border-radius:
+    ${input1} ${input2} ${input3} ${input4}`;
   };
 
   render() {
@@ -45,54 +46,39 @@ export default class BorderRadius extends Component {
     return (
       <div className="divBorder">
         <Header text="Border Radius Converter" show={true} />
-        <div className="borderRadious">
-          <div className="labelsBorder">
-            <label className="labelBorder" htmlFor="input1">
-              bottom-left
-              <input
-                value={input1}
-                className="inputBorder"
-                name="input1"
-                onInput={this.handleInput}
-                maxLength={10}
-              />
-            </label>
-            <label className="labelBorder" htmlFor="input2">
-              bottom-rigth
-              <input
-                value={input2}
-                className="inputBorder"
-                name="input2"
-                onInput={this.handleInput}
-                maxLength={10}
-              />
-            </label>
-            <label className="labelBorder" htmlFor="input3">
-              {/* the line below has some invisible unicode before top-left(2).*/}
-              ⠀⠀top-left
-              <input
-                value={input3}
-                className="inputBorder"
-                name="input3"
-                onInput={this.handleInput}
-                maxLength={10}
-              />
-            </label>
-            <label className="labelBorder" htmlFor="input4">
-              {/* the line below has some invisible unicode. before top-rigth(3)*/}
-              ⠀⠀⠀top-rigth
-              <input
-                value={input4}
-                className="inputBorder"
-                name="input4"
-                onInput={this.handleInput}
-                maxLength={10}
-              />
-            </label>
-          </div>
-        </div>
+        <div className="borderRadious"></div>
         <div className="divBorderMessage">
-          <div className="borderExample"></div>
+          <div className="borderExample">
+            <input
+              value={input1}
+              className="inputBorder"
+              name="input1"
+              onInput={this.handleInput}
+              maxLength={10}
+            />
+            <input
+              value={input2}
+              className="inputBorder"
+              name="input2"
+              onInput={this.handleInput}
+              maxLength={10}
+            />
+
+            <input
+              value={input3}
+              className="inputBorder"
+              name="input3"
+              onInput={this.handleInput}
+              maxLength={10}
+            />
+            <input
+              value={input4}
+              className="inputBorder"
+              name="input4"
+              onInput={this.handleInput}
+              maxLength={10}
+            />
+          </div>
           <div className="borderEx">
             <p className="message"></p>
             <button
@@ -105,7 +91,6 @@ export default class BorderRadius extends Component {
             </button>
           </div>
         </div>
-        <br />
         <Footer />
       </div>
     );
