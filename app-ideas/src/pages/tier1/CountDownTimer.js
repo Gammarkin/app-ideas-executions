9e7599fb642bec0ee35f726ff9fed3480cbd26ea
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-05-31 21:41:30 -0300

    made some test in the popover on mantine

--- app-ideas/src/pages/tier1/CountDownTimer.js
+++ app-ideas/src/pages/tier1/CountDownTimer.js
@@ -1,7 +1,43 @@
 import React, {Component} from 'react';
+import {Popover, Text, Button} from '@mantine/core';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 export default class CountDownTimer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showHiddenInfo: false,
+    };
+  }
   render() {
-    return <div>CountDownTimer</div>;
+    const {showHiddenInfo} = this.state;
+
+    return (
+      <div>
+        <Header text="Countdown Timer Event" show={true} />
+        <div>
+          <Popover
+            opened={showHiddenInfo}
+            onClose={() => this.setState({showHiddenInfo: false})}
+            target={
+              <Button onClick={() => this.setState({showHiddenInfo: true})}>
+                Details
+              </Button>
+            }
+            width={260}
+            position="bottom"
+            withArrow
+          >
+            <div style={{display: 'flex'}}>
+              <Text size="sm">
+                Thanks for stopping by and checking Mantine, you are awesome!
+              </Text>
+            </div>
+          </Popover>
+        </div>
+        <Footer />
+      </div>
+    );
   }
 }
