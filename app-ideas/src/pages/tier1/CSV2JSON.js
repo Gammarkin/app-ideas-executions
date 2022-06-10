a129489f4153b977bf5a0a9479b6ae2a8fb09204
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-06-09 22:12:20 -0300

    made csv i guess

--- app-ideas/src/pages/tier1/CSV2JSON.js
+++ app-ideas/src/pages/tier1/CSV2JSON.js
@@ -1,18 +1,77 @@
 import React, {useState} from 'react';
-import {Textarea} from '@mantine/core';
+import {Textarea, Button, Center} from '@mantine/core';
+import '../../styles/tier1-styles/CSV2JSON.css';
 
 export default function CSV2JSON() {
   const [error, setError] = useState('');
+  const [textAreaValue, setTextAreaValue] = useState('');
+  const [convertedResult, setConvertedResult] = useState({});
+
+  const handleVerification = () => {
+    if (!textAreaValue || !textAreaValue.includes(',')) {
+      setError('insert Valid CSV to be converted');
+      return false;
+    }
+    setError('');
+    return true;
+  };
+
+  const handleConversion = () => {
+    const isValid = handleVerification();
+
+    if (isValid) {
+      const convertedInArray = textAreaValue.split(', ');
+      let convertedJSON = [];
+
+      convertedInArray.forEach((converted) => {
+        convertedJSON = [...convertedJSON, converted];
+      });
+
+      setConvertedResult({...convertedJSON});
+      setTextAreaValue('');
+    }
+  };
+
+  const handleCopyJSON = () => {
+    const copy = JSON.stringify(convertedResult);
+    navigator.clipboard.writeText(copy);
+
+    setConvertedResult({});
+  };
+
   return (
     <div>
-      <Textarea
-        placeholder="Your comment"
-        label="Your comment"
-        radius="md"
-        size="xl"
-        required
-        error={error}
-      />
+      <Center>
+        <Textarea
+          onChange={({target: {value}}) => setTextAreaValue(value)}
+          placeholder="Lorem, Ipsum, Rocks "
+          label="Your CSV with an comma, and a space"
+          radius="md"
+          size="xl"
+          required
+          error={error}
+          value={textAreaValue}
+        />
+      </Center>
+      <Center>
+        <Button
+          style={{marginTop: '15px'}}
+          onClick={handleConversion}
+          color="red"
+          radius="md"
+        >
+          Convert
+        </Button>
+
+        <Button
+          style={{marginTop: '15px', marginLeft: '10px'}}
+          onClick={handleCopyJSON}
+          color="cyan"
+          radius="md"
+        >
+          Copy Result
+        </Button>
+      </Center>
     </div>
   );
 }
